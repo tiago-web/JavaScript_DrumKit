61d765c142ec46b0a03b01f6ae2f3dbd9b07aa62
Tiago Soriano <tiagosoriano2000@gmail.com>
2020-02-16 23:13:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
+// Detect Button press
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        var audio1 = new Audio('sounds/crash.mp3');
-        audio1.play();
+        var buttonHtml = this.innerHTML;
+        checkCase(buttonHtml);
         
     });
-    
 }
 
+// Detect Key press
+document.addEventListener("keydown", function(event){
+    var keyPress = event.key
+    checkCase(keyPress);
+
+});
+
+// Check cases
+function checkCase(event) {
+    switch (event) {
+        case 'w':
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'a':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 's':
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case 'j':
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'k':
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'l':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+    
+        default: console.log();
+            break;
+    }
+} 
